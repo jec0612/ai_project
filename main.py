1913a8cbc761766509534bb1a24f069eba976ca3
jec0612 <cis9443c3@gmail.com>
2026-05-13 13:45:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ st.title('나의 첫 웹 서비스 만들기!')
 a=st.text_input('이름을 입력해 주세요!') 
 b=st.selectbox('좋아하는 음식을 선택하세요!',['떡볶이','햄버거','튀김']) 
 if st.button('인사말 생성'):
-  st.write(a+'님!, 안녕하세요,반갑습니다!')
+  st.write(a+'님!, 안녕하세요,반갑습니다!') 
+  st.info('반갑습니다')
+  st.warning(b+'음식을 좋아하시는군요')
+  st.error('잘부탁해요!') 
